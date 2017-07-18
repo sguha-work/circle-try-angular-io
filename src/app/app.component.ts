71397f3458564b032e9d80c9dda9d0950bea4f07
Sahasrangshu Guha <sahasrangshu.guha@ericsson.com>
2017-07-18 19:49:05 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgCircleProgressModule } from 'ng-circle-progress';
+//import { NgCircleProgressModule } from 'ng-circle-progress';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
